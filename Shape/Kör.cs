24b2a29b1c910e6f6d7c70ecf45897f975f88505
Petrik Diak <diak@petrik.hu>
2025-03-21 12:28:10 +0100

    Órai munka

---
+++ Shape/Kör.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape
+{
+	internal class Kör : Shape
+	{
+		double r;
+
+
+		public Kör(string color, double r, bool isHoley = false) : base(color, isHoley)
+		{
+			this.r = r;
+		}
+
+		public override double Area()
+		{
+			return Math.Pow(r, 2) * Math.PI; ;
+		}
+
+		public override double Perimeter()
+		{
+			return 2 * r * Math.PI; ;
+		}
+
+		public override string? ToString()
+		{
+			return base.ToString() + " - kör";
+		}
+	}
+}
--- Shape/Program.cs
+++ Shape/Program.cs
@@ -4,7 +4,19 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
-		}
+			Rectangle rec = new Rectangle(false, "red", 2, 3);
+            Console.WriteLine(rec.ToString());
+
+			Shape[] shapes =
+			{
+				rec,
+				new Kocka(true, "blue", 6, 5),
+				new Kör("fekete", 5)
+			};
+			//Kocka kocka = new Kocka(false, "red", 4, 6);
+			//Shape shape3 = kocka;
+
+
+        }
 	}
 }
