e09d9bebdaa1816cd466db74392e7ad651aba9d7
Petrik Diak <diak@petrik.hu>
2025-03-21 13:10:00 +0100

    Feladat kész

---
+++ Shape/Haromszog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape
+{
+	internal class Haromszog : Shape
+	{
+		double hossz;
+		double height;
+		double width;
+		public Haromszog(string color,double hossz,double width, double height, bool isHoley = false) : base(color, isHoley)
+		{
+		}
+
+		public override double Area()
+		{
+			return height * hossz;
+		}
+
+		public override double Perimeter()//
+		{
+			return hossz+height+width;
+		}
+
+		public override string? ToString()
+		{
+			return base.ToString() + " - haromszog";
+		}
+	}
+}
--- Shape/Program.cs
+++ Shape/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Shape
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Shape
 {
 	internal class Program
 	{
@@ -10,13 +13,64 @@
 			Shape[] shapes =
 			{
 				rec,
-				new Kocka(true, "blue", 6, 5),
-				new Kör("fekete", 5)
+				new Kocka(true, "blue", 5, 5),
+				new Kör("fekete", 5),
+				new Rectangle(true, "szürke", 6, 9),
+				new Kör("lila", 10), 
 			};
-			//Kocka kocka = new Kocka(false, "red", 4, 6);
-			//Shape shape3 = kocka;
+			for (int i = 0; i < shapes.Length; i++)
+			{
+				Console.WriteLine(shapes[i].ToString());
+			}
+			Console.WriteLine();
+            //Kocka kocka = new Kocka(false, "red", 4, 6);
+            //Shape shape3 = kocka;
+            Lyuk(shapes);
+            Console.WriteLine();
+
+            Console.WriteLine(kell(5, 10).ToString());
+            Console.WriteLine();
+            Legnagyobb(shapes);
+
+
 
 
         }
+		static void Lyuk(Shape[] shapes)
+		{
+			for (int i = 0; i < shapes.Length; i++)
+			{
+				if (shapes[i].Area()> shapes[i].Perimeter() )
+				{
+					shapes[i].MakeHoley();
+					Console.WriteLine($" a {shapes[i].ToString()} lyukas lett");
+				}
+			}
+        }
+
+		static Shape kell(double hpssz, double masik)
+		{ 
+			if(hpssz ==  masik )
+			{
+
+				return new Kocka(false, "szürke", 5, 5);
+			}
+			return new Rectangle(true, "fehér", 5, 10);
+		}
+
+		static void Legnagyobb(Shape[] shapes)
+		{
+			Shape alma = shapes[0];
+			for (int i = 0;i < shapes.Length;i++)
+			{
+				if (shapes[i].Area()> alma.Area())
+				{
+					alma = shapes[i];
+				}
+			}
+            Console.WriteLine(alma.ToString());
+        }
+
+
 	}
 }
